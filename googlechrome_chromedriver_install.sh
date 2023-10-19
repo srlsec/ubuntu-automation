908e7da7743a13239116bd0ebaecb5c314d1c95f
srlsec <sarathlalupsrl@gmail.com>
2023-10-19 07:18:41 +0530

    23.10.19

---
+++ googlechrome_chromedriver_install.sh
@@ -0,0 +1,7 @@
+
+
+sudo apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome*.deb
+rm google-chrome*.deb
+google-chrome --version
